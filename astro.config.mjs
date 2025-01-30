a3840503440bf3927faa3d00cec36147bd594318
Tim Eaton <tim@yakdrive.io>
2025-01-30 18:30:54 +0100

    feat: added @playform/compress to speed up site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
+import { CONFIG } from "./src/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
   base: "/",
-  integrations: [sitemap(), mdx()],
+  site: CONFIG.site_url,
+  integrations: [sitemap(), mdx(), playformCompress()],
   markdown: {
     shikiConfig: {
       theme: "material-theme-darker",
